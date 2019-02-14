be24815faf147b1d67b05288a6132a8ea4d06310
loranBepatient <laurent.muller@bepatient.com>
2019-02-14 17:04:23 +0100

    Add a simple method to the component

--- app/navbar-component/navbar.component.js
+++ app/navbar-component/navbar.component.js
@@ -6,12 +6,22 @@
 
   function controller() {
     var ctrl = this;
+    ctrl.onClick = onClick;
     ctrl.links = [
       {label: 'Home', link: '#', active: true, disabled: false},
       {label: 'Features', link: '#', active: false, disabled: false},
       {label: 'Pricing', link: '#', active: false, disabled: false},
       {label: 'Disabled', link: '#', active: false, disabled: true},
     ]
+
+    function onClick(index) {
+      ctrl.link = ctrl.links.map(function(link, i) {
+        link.active = index === i
+        return link
+      })
+
+    }
+
   }
   angular.module('TodosApp').component('navBar', component)
 })()
