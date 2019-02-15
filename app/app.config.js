39bbbfe00a9d8de5a5cd8e88a8e91a01408b3962
LoranM <laurentm@me.com>
2019-02-15 05:10:37 +0100

    add route config

--- app/app.config.js
+++ app/app.config.js
@@ -1,24 +1,11 @@
 (function() {
   function routes($stateProvider) {
-    var todos = {
-      name: "todos",
-      url: "/todos",
-      component: "todosComponent",
-      resolve: {
-        todos: getTodos
-      }
-    };
     var features = {
       name: "features",
       url: "/features",
       component: "featuresComponent"
     };
 
-    function getTodos(TodosService) {
-      return TodosService.getTodos();
-    }
-
-    $stateProvider.state(todos);
     $stateProvider.state(features);
   }
   angular.module("TodosApp").config(routes);
---
+++ app/todos/todos.config.js
@@ -0,0 +1,18 @@
+(function() {
+  function routes(stateProvider) {
+    var todos = {
+      name: "todos",
+      url: "/todos",
+      component: "todosComponent",
+      resolve: {
+        todos: getTodos
+      }
+    };
+    function getTodos(TodosService) {
+      return TodosService.getTodos();
+    }
+
+    $stateProvider.state(todos);
+  }
+  angular.module("TodosModule").config(routes);
+})();
