d10f7ba83bd362e496a689f6682583b2515efe57
LoranM <laurentm@me.com>
2019-02-15 03:16:44 +0100

    add simple service

--- app/todos-component/todos.component.js
+++ app/todos-component/todos.component.js
@@ -4,63 +4,19 @@
     controller: controller
   };
 
-  function controller() {
+  function controller(TodosService) {
     var ctrl = this;
     ctrl.title = "My Todos";
     ctrl.query = "";
+    ctrl.todos = [];
     ctrl.onDelete = onDelete;
     ctrl.onCompleted = onCompleted;
-    ctrl.todos = [
-      {
-        userId: 1,
-        id: 1,
-        title: "delectus aut autem",
-        completed: true
-      },
-      {
-        userId: 1,
-        id: 2,
-        title: "quis ut nam facilis et officia qui",
-        completed: false
-      },
-      {
-        userId: 1,
-        id: 3,
-        title: "fugiat veniam minus",
-        completed: false
-      },
-      {
-        userId: 1,
-        id: 4,
-        title: "et porro tempora",
-        completed: true
-      },
-      {
-        userId: 1,
-        id: 5,
-        title:
-          "laboriosam mollitia et enim quasi adipisci quia provident illum",
-        completed: false
-      },
-      {
-        userId: 1,
-        id: 6,
-        title: "qui ullam ratione quibusdam voluptatem quia omnis",
-        completed: false
-      },
-      {
-        userId: 1,
-        id: 7,
-        title: "illo expedita consequatur quia in",
-        completed: false
-      },
-      {
-        userId: 1,
-        id: 8,
-        title: "quo adipisci enim quam ut ab",
-        completed: true
-      }
-    ];
+
+    ctrl.$onInit = onInit;
+
+    function onInit() {
+      ctrl.todos = TodosService.getTodos();
+    }
 
     function onDelete(todo) {
       ctrl.todos = ctrl.todos.filter(function(_todo) {
